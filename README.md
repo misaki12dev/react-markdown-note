641490ed70c9553e95de099f3445d4392a8bfc8e
misaki12dev <misaki.12k@gmail.com>
2022-09-09 16:11:30 +0900

    saved to localstrage

--- README.md
+++ README.md
@@ -2,7 +2,12 @@
 
 **Installed Packages**
 
-nanoid(unique ID generator): https://www.npmjs.com/package/nanoid <br/>
-react-mde(markdown editor for react) : https://www.npmjs.com/package/react-mde<br/>
-showdown(Javascript Markdown to HTML converter) : https://www.npmjs.com/package/showdown<br/>
-react-split(create a resizeable split view) : https://www.npmjs.com/package/react-split
+- nanoid(unique ID generator): https://www.npmjs.com/package/nanoid
+- react-mde(markdown editor for react) : https://www.npmjs.com/package/react-mde
+- showdown(Javascript Markdown to HTML converter) : https://www.npmjs.com/package/showdown
+- react-split(create a resizeable split view) : https://www.npmjs.com/package/react-split
+
+
+**used methods**
+- getItem()
+-
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "./components/Sidebar";
 import Editor from "./components/Editor";
 import { data } from "./data";
@@ -6,19 +6,16 @@ import Split from "react-split";
 import { nanoid } from "nanoid";
 
 export default function App() {
-  /**
-   * Challenge:
-   * 1. Every time the `notes` array changes, save it
-   *    in localStorage. You'll need to use JSON.stringify()
-   *    to turn the array into a string to save in localStorage.
-   * 2. When the app first loads, initialize the notes state
-   *    with the notes saved in localStorage. You'll need to
-   *    use JSON.parse() to turn the stringified array back
-   *    into a real JS array.
-   */
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("notes")) || []
+  );
 
-  const [notes, setNotes] = React.useState(undefined || []);
-  const [currentNoteId, setCurrentNoteId] = React.useState(
+  useEffect(() => {
+    //localstrageに保存 setItem()
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  const [currentNoteId, setCurrentNoteId] = useState(
     (notes[0] && notes[0].id) || ""
   );
 
